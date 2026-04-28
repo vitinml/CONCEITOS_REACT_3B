ec61561ef6958ef83420f5a686b76450f3d6eef6
VITOR MARTINHO LOFI <vitor_lofi@estudante.sesisenai.org.br>
2026-04-28 12:24:09 -0300

    pi

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const renderizarTela = () => {
+    //navegação
+  }
+
+  return (
+    <>
+      <section id="center">
+       
+      </section>
+    </>
+  )
+}
+
+export default App
