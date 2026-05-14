f0089feb6af0c4d4fa42888a3af083ba5abbd4e0
VITOR MARTINHO LOFI <vitor_lofi@estudante.sesisenai.org.br>
2026-05-14 12:01:15 -0300

    pa

--- src/paginas/Usuario.jsx
+++ src/paginas/Usuario.jsx
@@ -2,9 +2,18 @@ import { useEffect, useState } from "react"
 
 export default function Usuario () {
    const [contador, setContador] = useState(0)
+   const [usuarios, setUsuarios] = useState([])
 
    useEffect(() => {
          document.title = 'contagem ' + contador
+         const buscarUsuarios = async () => {
+            const resposta = await fetch('http://localhost:3000/usuarios')
+            const data = await resposta.json()
+            setUsuarios(data)
+            console.log(data);
+         }
+         buscarUsuarios()
+
    }, [contador])
 
    return (
@@ -13,6 +22,15 @@ export default function Usuario () {
          <h1>{contador}</h1>
 
          <button onClick={() => {setContador(contador + 1)}}>somar</button>
+
+         <ul>
+         {usuarios.map( (usuario) => (
+            <li key={usuario.id}>
+                  <b>{usuario.nome}</b>
+                  status: { usuario.ativo ? 'Ativo' : 'Desativo' }
+            </li>
+         ))}
+         </ul>
       </div>
    ) 
 }
