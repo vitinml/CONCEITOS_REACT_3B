09cfe1654b5329f845aa8f151344e3247f864c93
VITOR MARTINHO LOFI <vitor_lofi@estudante.sesisenai.org.br>
2026-05-19 12:38:41 -0300

    formulario

--- src/paginas/Usuario.jsx
+++ src/paginas/Usuario.jsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react"
 
 export default function Usuario () {
    const [contador, setContador] = useState(0)
+   const [email, setEmail] = useState('')
+    const [nome, setNome] = useState("")
+     const [senha, setSenha] = useState('')
    const [usuarios, setUsuarios] = useState([])
 
    useEffect(() => {
@@ -22,6 +25,26 @@ export default function Usuario () {
          <h1>{contador}</h1>
 
          <button onClick={() => {setContador(contador + 1)}}>somar</button>
+
+      <input type="text" id="email"
+      placeholder="DIGITE EMAIL"
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+      />
+      <input type="text" id="nome"
+      placeholder="DIGITE NOME"
+      value={nome}
+      onChange={(e) => setNome(e.target.value)}
+      />
+      <input type="text" id="senha"
+      placeholder="DIGITE SENHA"
+      value={senha}
+      onChange={(e) => setSenha(e.target.value)}
+      />
+
+      <h3>{email}</h3>
+      <h4>{nome}</h4>
+      <h5>{senha}</h5>
 
          <ul>
          {usuarios.map( (usuario) => (
